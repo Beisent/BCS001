6e190139e504eb7edbfc2fd856946a25154e0358
Yang-Junjie <1973690778@qq.com>
2025-11-28 00:55:44 +0800

    还没写完先交一个

---
+++ Learn-Computer-Graphics-From-Scratch/Code/02/02.2/src/main.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+static const int width = 256;
+static const int height = 256;
+int size = width * height;
+
+int *r = new int[size];
+int *g = new int[size];
+int *b = new int[size];
+
+void SetPixel(int x, int y, int ColorR, int ColorG, int ColorB)
+{
+    int index = x + y * width;
+    r[index] = ColorR;
+    g[index] = ColorG;
+    b[index] = ColorB;
+}
+
+bool Render(std::string filename)
+{
+    std::ofstream f(filename);
+    if (!f)
+    {
+        std::cerr << "Error: Cannot open file for writing." << std::endl;
+        return false;
+    }
+
+    f << "P3\n"
+      << width << ' ' << height << "\n255\n";
+    for (int i = 0; i < size; ++i)
+    {
+        f << r[i] << ' ' << g[i] << ' ' << b[i] << ' ';
+    }
+    f << '\n';
+
+    std::cout << "PPM image written to output.ppm" << std::endl;
+    return true;
+}
+
+void BresenhamLine(int x0, int y0, int x1, int y1, int R, int G, int B)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    int d = 2 * dy - dx;   
+    int y = y0;
+
+    for (int x = x0; x <= x1; x++)
+    {
+        SetPixel(x, y, R, G, B);
+
+        if (d > 0)
+        {
+            y += 1;
+            d -= 2 * dx;
+        }
+
+        d += 2 * dy;
+    }
+}
+int main()
+{
+
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            SetPixel(x, y, 0, 0, 0);
+        }
+    }
+    BresenhamLine(0, 0, 100, 49, 255, 0, 0);
+    Render("output.ppm");
+
+    delete[] r;
+    delete[] g;
+    delete[] b;
+    return 0;
+}
